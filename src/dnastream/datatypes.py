57f846325b5d700bce718e3f5272a83d3e3c7c8c
Leah Weber <llweber@edragon120.cm.cluster>
2025-03-07 14:49:38 -0600

    separated out schema and datatypes into separate files

---
+++ src/dnastream/datatypes.py
@@ -0,0 +1,62 @@
+import h5py 
+import numpy as np
+
+
+# datatype for metadata for SNV index
+SNV_DTYPE = np.dtype([
+                    ("label", h5py.string_dtype(encoding="utf-8")),
+                    ("chrom", "S5"),  # Fixed-length string (5 characters max)
+                    ("pos", "i8"), 
+                    ("end_pos", "i8"),    # Integer
+                    ("ref_allele", "S1"),  # Single-character string
+                    ("alt_allele", "S1"),  # Single-character string
+                    ("hugo", "S15"), # Fixed-length string (15 characters max)
+                    ("gene", "S10"), #Fixed-length string (10 characters ma)       
+             ])
+
+#datatype to store metadata for sample index
+SAMPLE_DTYPE = np.dtype([
+                    ("label", h5py.string_dtype(encoding="utf-8")),  #sample description
+                    ("patient", "S10"),  # Fixed-length string (10 characters max)
+                    ("source", "S10"),  # Fixed-length string (10 characters max)
+                    ("location", "S15") , # Fixed-length string (15 characters max)
+                    ("bam_file", h5py.string_dtype(encoding="utf-8")),  # Single-character strin
+])
+
+#datatype to for the index modification logs
+LOG_DTYPE = np.dtype([
+            ("timestamp", h5py.string_dtype(encoding="utf-8")),
+            ("number", "i8"),
+            ("index_size_before", "i8"),
+            ("index_size_after", "i8"),
+            ("operation", "S15"),
+            ("user", "S15"),
+            ("hostname", "S15"),
+            ("file",  h5py.string_dtype(encoding="utf-8"))
+        ])
+
+#datatype for tree edge list
+EDGE_LIST_DTYPE = np.dtype([
+            ("parent", "i4"),
+            ("child", "i4")
+        ])
+
+#datatype for storing tree metadata
+TREE_DTYPE = np.dtype([
+    ("label", "S15"),
+    ("method", "S15"),
+    ("score", "f8"),
+    ("rank", "i4")
+    ])
+
+VLEN_EDGE_DTYPE = h5py.special_dtype(vlen=EDGE_LIST_DTYPE)
+
+#datatype for the dataset track log
+DATASET_LOG_DTYPE = np.dtype([
+            ("timestamp", h5py.string_dtype(encoding="utf-8")),  # Timestamp
+            ("dataset", h5py.string_dtype(encoding="utf-8")),   # Dataset path modified
+            ("operation", "S15"),  # Operation type: "add", "update", "delete"
+            ("user", "S15"),  # User who performed the modification
+            ("hostname", "S15"),  # Host machine
+            ("file", h5py.string_dtype(encoding="utf-8")),  # Host machine
+])
---
+++ src/dnastream/schema.py
@@ -0,0 +1,61 @@
+import h5py 
+from .datatypes import LOG_DTYPE, SNV_DTYPE, SAMPLE_DTYPE, VLEN_EDGE_DTYPE, DATASET_LOG_DTYPE, TREE_DTYPE
+
+
+MODALITIES = ["bulk", "lcm", "scdna"]
+
+STRUCT_ARRAYS = ["log", "data"]
+
+META_TABLES = ["data", "label", "cluster", "index_map", "log"]
+
+
+
+READ_COUNTS = {f"read_counts/{m}/{c}":  "i" for c in ["variant", "total"] for m in MODALITIES}
+    
+SCEHMA = {  "metadata" : 
+                {
+                    "log" : DATASET_LOG_DTYPE
+                },
+            "SNV": 
+                {"label" :  h5py.string_dtype(encoding="utf-8"),
+                "cluster" : "i8",
+                "data" : SNV_DTYPE,
+                "index_map": h5py.string_dtype("utf-8"),
+                "log" : LOG_DTYPE,
+                },
+            "sample" : 
+                {
+                "label":    h5py.string_dtype(encoding="utf-8") ,
+                "cluster" : "i8",
+                "data" : SAMPLE_DTYPE,
+                "index_map":    h5py.string_dtype("utf-8"),
+                "log" : LOG_DTYPE
+                },
+            "tree" :
+                {
+                    "SNV_trees" : 
+                       {
+                           "trees" : VLEN_EDGE_DTYPE,
+                           "data"  : TREE_DTYPE,
+                           "index_map" : h5py.string_dtype("utf-8"),
+                       },
+                    "CNA_trees" : 
+                       {
+                           "trees" : VLEN_EDGE_DTYPE,
+                           "data"  : TREE_DTYPE,
+                           "index_map" : h5py.string_dtype("utf-8"),
+                       },
+                    "clonal_trees" : 
+                       {
+                           "trees" : VLEN_EDGE_DTYPE,
+                           "data"  : TREE_DTYPE,
+                           "index_map" : h5py.string_dtype("utf-8"),
+                       },
+                }
+   
+    }
+
+
+
+
+
