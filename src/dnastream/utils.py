c55af553332c6833fb792647bdb52ccbb5900d67
Leah Weber <llweber@edragon200.cm.cluster>
2025-05-12 14:07:59 -0500

    refactored code to include utility script and IOMixin for all I/O functions

---
+++ src/dnastream/utils.py
@@ -0,0 +1,27 @@
+import pathlib
+import time
+import functools
+
+def wrap_list(val):
+    if type(val) is list:
+        return val
+    return [val]
+
+
+def full_path(fname):
+    return str(pathlib.Path(fname).resolve())
+
+
+def timeit(func):
+    """Decorator to measure execution time of a function."""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()  # Start timer
+        result = func(*args, **kwargs)  # Run the function
+        end_time = time.perf_counter()  # End timer
+        elapsed_time = end_time - start_time
+        print(f"⏱ Function '{func.__name__}' took {elapsed_time:.4f} seconds")
+        return result
+
+    return wrapper
